c5aa6c895a657731909a4ea8bc0a34dd28d62297
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-05-23 16:28:59 -0700

    added name column to devise (user) table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+
+ before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation]
+  end
+  
 end
