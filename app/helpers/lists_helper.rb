1c84a94a8d67ce14ec1a8ca97d7cc46bf47621a2
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-06-03 20:36:28 -0700

    latest update

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,11 +1,16 @@
 module ListsHelper
 
+	def item_list(list)
+		@item_list = Item.where list_id: @list.id
+	end
+
+
+
 	def find_number_of_items(list)
 		@list = list
-		@item_list = Item.where list_id: @list.id
 		@total = 0
 
-		@item_list.each do |x|
+		item_list(@list).each do |x|
 			@total += x.qty
 		end
 
@@ -15,14 +20,14 @@ module ListsHelper
 
 	def find_total_price(list)
 		@list = list
-		@item_list = Item.where list_id: @list.id
 		@price_total = 0
 
-		@item_list.each do |x|
+		item_list(@list).each do |x|
 			@total += (x.price * x.qty)
 		end
 
 		return @total
 	end
+
 
 end
